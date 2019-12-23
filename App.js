3168b3574d37c430a62a134c210a06b0b5565af6
Scott Young <youngscottj@gmail.com>
2019-12-23 08:34:10 -0700

    add Amplify auth

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import amplify from './aws-exports';
+Amplify.configure(amplify)
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
